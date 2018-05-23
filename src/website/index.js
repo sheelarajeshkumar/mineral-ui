dd4fd6d21721444b4e5a89740bf47e1ab9c3225c
Brent Ertz <brent.ertz@ca.com>
2018-05-23 12:46:16 -0600

    WIP: Delete test AppEmotion

--- src/website/index.js
+++ src/website/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import 'babel-polyfill';
 import ThemeProvider from '../library/themes/ThemeProvider';
 import App from './app/App';
-// import App from './app/AppEmotion';
 import demoRoutes from './app/demos/routes';
 require('./index.css');
 
