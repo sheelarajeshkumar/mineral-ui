57b2d82935d34b566d50b8d54d4bc66b75219c70
kylegach <kyle.gach@ca.com>
2018-07-01 18:25:52 -0600

    WIP: Adjust row background colors to match spec

--- src/library/Table/TableRow.js
+++ src/library/Table/TableRow.js
@@ -12,12 +12,12 @@ type Props = {
 
 // prettier-ignore
 export const componentTheme = (baseTheme: Object) => ({
-  TableRow_backgroundColor_hover: baseTheme.color_gray_30,
-  TableRow_backgroundColor_selected: baseTheme.color_theme_20,
-  TableRow_backgroundColor_selectedHover: baseTheme.color_theme_30,
-  TableRow_backgroundColor_highContrast_selected: baseTheme.color_theme_30,
-  TableRow_backgroundColor_highContrast_selectedHover: baseTheme.color_theme_40,
-  TableRow_backgroundColor_striped: baseTheme.color_gray_20,
+  TableRow_backgroundColor_hover: baseTheme.color_gray_20,
+  TableRow_backgroundColor_selected: baseTheme.color_theme_10,
+  TableRow_backgroundColor_selectedHover: baseTheme.color_theme_20,
+  TableRow_backgroundColor_highContrast_selected: baseTheme.color_theme_20,
+  TableRow_backgroundColor_highContrast_selectedHover: baseTheme.color_theme_30,
+  TableRow_backgroundColor_striped: baseTheme.color_gray_10,
   TableRow_borderHorizontal: `1px solid ${baseTheme.color_white}`,
   TableRow_borderHorizontal_highContrast: `1px solid ${baseTheme.color_gray_60}`,
 
