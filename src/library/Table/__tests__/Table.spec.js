b0ad308ccd1d1e2c77427c9ba025ba20f827954a
Brent Ertz <brent.ertz@ca.com>
2018-07-10 11:27:40 -0600

    WIP: Add Table smoke test

--- src/library/Table/__tests__/Table.spec.js
+++ src/library/Table/__tests__/Table.spec.js
@@ -1,23 +1,25 @@
 /* @flow */
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Table from '../Table';
 import examples from '../../../website/app/demos/Table/examples';
 import testDemoExamples from '../../../../utils/testDemoExamples';
 
-// function shallowTable(props = {}) {
-//   return shallow(
-//     <Table {...props}>
-//
-//     </Table>
-//   );
-// }
+function shallowTable(props = {}) {
+  const tableProps = {
+    data: [],
+    title: 'Test',
+    ...props
+  };
+  return shallow(<Table {...tableProps} />);
+}
 
 describe('Table', () => {
   testDemoExamples(examples);
 
-  // it('renders', () => {
-  //   const table = shallowTable();
-  //
-  //   expect(table.exists()).toEqual(true);
-  // });
+  it('renders', () => {
+    const table = shallowTable();
+
+    expect(table.exists()).toEqual(true);
+  });
 });
